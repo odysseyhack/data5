18e7e06b20680719b32d5f067d061769f5ea26a6
Johnny637 <johnny.baas@xmsnet.nl>
2019-06-28 22:19:00 +0200

    Delete IDataExtractions.cs

--- DatasetDownloader/BusinessLogic/IDataExtractions.cs
+++
@@ -1,9 +0,0 @@
-﻿using DatasetDownloader.DataContracts;
-
-namespace DatasetDownloader.BusinessLogic
-{
-    public interface IDataExtractions
-    {
-        DataFieldMain ExecuteDataInformationExtraction(string[] items, string delimiter, string filename);
-    }
-}
